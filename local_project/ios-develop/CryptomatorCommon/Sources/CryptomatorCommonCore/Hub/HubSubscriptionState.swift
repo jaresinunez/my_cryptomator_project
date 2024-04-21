e24bf34efe512744b54965ea4dc5366375291b1e
Jaresi Nunez <58448481+jaresinunez@users.noreply.github.com>
2024-04-21 15:47:12 -0500

    Added image to launch screeen to create splash screen

--- local_project/ios-develop/CryptomatorCommon/Sources/CryptomatorCommonCore/Hub/HubSubscriptionState.swift
+++
@@ -1,4 +0,0 @@
-public enum HubSubscriptionState: String, Codable {
-	case active
-	case inactive
-}
--- local_project/ios-develop/CryptomatorCommon/Sources/CryptomatorCommonCore/Promise+StructuredConcurrency.swift
+++
@@ -1,9 +0,0 @@
-import Promises
-
-public extension Promise {
-	func getValue() async throws -> Value {
-		try await withCheckedThrowingContinuation({ continuation in
-			self.then(continuation.resume(returning:)).catch(continuation.resume(throwing:))
-		})
-	}
-}
